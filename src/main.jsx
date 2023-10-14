d4208383f0b71bb3c7d0431b67d230c4e85f4bd2
Qrin <61843273+Qrinee@users.noreply.github.com>
2023-10-14 11:34:51 +0200

    test

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ import ErrorPage from './pages/ErrorPage.jsx'
 
 const router = createBrowserRouter([
   {
-    path: "/test",
+    path: "/",
     element: <MainPage/>,
     errorElement: <ErrorPage/>
   },
